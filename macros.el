b5e7738cc797285e084cc06c60c6d65c5aec6521
ivanbrennan <ivan.brennan@gmail.com>
2017-01-15 00:04:49 -0500

    fix a double-digits sorting bug
    This hadn't cropped up yet, as the refactorings never reached
    double-digit indices, but the next (Inline Class) does, and will get
    processed in the wrong order if I don't fix these function names to use double-digits.

--- macros.el
+++ macros.el
@@ -1,55 +1,55 @@
 ;;; Move Method
-(fset 'refactor-move-method-0
+(fset 'refactor-move-method-00
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([105 99 108 97 115 115 32 65 99 99 111 117 110 116 return 100 101 102 32 111 118 101 114 100 114 97 102 116 95 99 104 97 114 103 101 return 105 102 32 64 97 99 99 111 117 110 116 95 116 121 112 101 46 112 114 101 109 105 117 109 63 return 114 101 115 117 108 116 32 61 32 49 48 return 114 101 115 117 108 116 32 43 61 32 40 64 100 97 121 115 95 111 118 101 114 100 114 97 119 110 32 45 32 55 41 32 42 32 48 46 56 53 32 105 102 32 64 100 97 121 115 95 111 118 101 114 100 114 97 119 110 32 62 32 55 return 114 101 115 117 108 116 return 101 108 115 101 return 64 100 97 121 115 95 111 118 101 114 100 114 97 119 110 32 42 32 49 46 55 53 escape 106 106 111 return 100 101 102 32 98 97 110 107 95 99 104 97 114 103 101 return 114 101 115 117 108 116 32 61 32 52 46 53 return 114 101 115 117 108 116 32 43 61 32 111 118 101 114 100 114 97 102 116 95 99 104 97 114 103 101 32 105 102 32 64 100 97 121 115 95 111 118 101 114 100 114 97 119 110 32 62 32 48 return 114 101 115 117 108 116 escape 106 106 111 return 99 108 97 115 115 32 65 99 99 111 117 110 116 84 121 112 101 escape] 0 "%d")) arg)))
-(fset 'refactor-move-method-1
+(fset 'refactor-move-method-01
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217788 134217747 94 99 108 97 115 115 32 65 99 99 111 117 110 116 36 return 134217747 94 32 32 134217753 134217753 134217753 134217753 23 36 return 121 121 134217747 94 99 108 97 115 115 32 23 36 return 112 111 101 110 100 escape] 0 "%d")) arg)))
-(fset 'refactor-move-method-2
+(fset 'refactor-move-method-02
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217788 19 134217842 94 99 108 97 115 115 32 65 99 99 111 117 110 116 36 return 19 134217842 94 32 32 100 101 102 32 111 118 101 114 23 36 return 86 37 107 79 106 121 134217747 94 99 108 97 115 115 32 65 23 36 return 134217747 94 32 32 23 23 36 return 112] 0 "%d")) arg)))
-(fset 'refactor-move-method-3
+(fset 'refactor-move-method-03
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217788 134217747 94 99 108 97 115 115 32 65 99 99 111 117 110 116 84 23 36 return 134217747 94 32 32 23 23 36 return 19 64 23 return 100 102 46] 0 "%d")) arg)))
-(fset 'refactor-move-method-4
+(fset 'refactor-move-method-04
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([47 64 return 120 110 120 110 120 134217746 94 32 32 100 101 102 32 111 118 101 114 100 114 97 102 116 95 99 104 97 114 103 101 36 return 65 40 100 97 121 115 95 111 118 101 114 100 114 97 119 110 escape] 0 "%d")) arg)))
-(fset 'refactor-move-method-5
+(fset 'refactor-move-method-05
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217788 134217747 94 23 23 36 return 134217747 94 32 32 23 23 36 return 86 37 107 79 106 115 64 97 99 99 111 117 110 116 95 116 121 112 101 46 111 118 101 114 100 114 97 102 116 95 99 104 97 114 103 101 40 64 100 97 121 115 95 111 118 101 114 100 114 97 119 110 escape] 0 "%d")) arg)))
-(fset 'refactor-move-method-6
+(fset 'refactor-move-method-06
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217788 134217747 94 23 23 36 return 134217747 94 32 32 100 101 102 32 98 23 36 return 134217747 105 102 32 64 100 97 121 115 95 111 118 101 114 100 114 97 119 110 return 105 return C-return 101 110 100 escape 107 107 100 100 112 61 61 19 111 118 101 114 return 105 64 97 99 99 111 117 110 116 95 116 121 112 101 46 escape 65 40 64 100 97 121 115 95 111 118 100 backspace 101 114 100 114 97 119 110 escape] 0 "%d")) arg)))
-(fset 'refactor-move-method-7
+(fset 'refactor-move-method-07
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217788 134217747 94 23 23 36 return 134217747 94 32 32 100 101 102 23 36 return 86 37 106 100] 0 "%d")) arg)))
-(fset 'refactor-move-method-8
+(fset 'refactor-move-method-08
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217788 134217747 94 23 23 36 return 134217747 94 32 32 23 23 36 return 134217747 64 97 99 99 111 117 110 116 23 23 23 23 41 return 102 40 108 99 105 40 115 101 108 102 escape 134217747 94 99 108 97 115 115 23 36 return 134217747 94 32 32 23 23 return 102 40 108 99 105 40 97 99 99 111 117 110 116 escape 134217747 100 97 121 115 23 return 105 97 99 99 111 117 110 116 46 escape 108 19 23 19 return 105 97 99 99 111 117 110 116 46 escape 19 19 19 return 105 97 99 99 111 117 110 116 46 escape] 0 "%d")) arg)))
-(fset 'refactor-move-method-9
+(fset 'refactor-move-method-09
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217788 134217747 94 99 108 97 115 115 32 65 99 99 111 117 110 116 84 121 112 101 36 return 134217747 94 32 32 100 101 102 23 return 19 105 102 32 97 99 99 111 117 110 116 return 105 return C-return 101 110 100 escape 107 107 100 100 112 61 61] 0 "%d")) arg)))
 
 ;;; Move Field
-(fset 'refactor-move-field-0
+(fset 'refactor-move-field-00
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([105 99 108 97 115 115 32 65 99 99 111 117 110 116 escape 111 100 101 102 32 105 110 116 101 114 101 115 116 95 102 111 114 95 97 109 111 117 110 116 95 100 97 121 115 40 97 109 111 117 110 116 44 32 100 97 121 115 41 return 64 105 110 116 101 114 101 115 116 95 114 97 116 101 32 42 32 97 109 111 117 110 116 32 42 32 100 97 121 115 32 47 32 51 54 53 escape] 0 "%d")) arg)))
-(fset 'refactor-move-field-1
+(fset 'refactor-move-field-01
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([103 103 111 97 116 116 114 95 114 101 97 100 101 114 32 58 105 110 116 101 114 101 115 116 95 114 97 116 101 escape C-return 19 64 return 120] 0 "%d")) arg)))
-(fset 'refactor-move-field-2
+(fset 'refactor-move-field-02
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([103 103 106 83 100 101 102 32 105 110 116 101 114 101 115 116 95 114 97 116 101 return 64 97 99 99 111 117 110 116 95 116 121 112 101 46 105 110 116 101 114 101 115 116 95 114 97 116 101 escape] 0 "%d")) arg)))
-(fset 'refactor-move-field-3
+(fset 'refactor-move-field-03
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([103 103 79 114 101 113 117 105 114 101 32 39 102 111 114 119 97 114 100 97 98 108 101 escape C-return 106 106 111 101 120 116 101 110 100 32 70 111 114 119 97 114 100 97 98 108 101 escape C-return 106 106 99 50 106 100 101 102 95 100 101 108 101 103 97 116 111 114 32 58 64 97 99 99 111 117 110 116 95 116 121 112 101 44 32 58 105 110 116 101 114 101 115 116 95 114 97 116 101 escape] 0 "%d")) arg)))
 
 ;;; Extract Class
-(fset 'refactor-extract-class-0
+(fset 'refactor-extract-class-00
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([105 99 108 97 115 115 32 80 101 114 115 111 110 escape 111 97 116 116 114 95 114 101 97 100 101 114 32 58 110 97 109 101 escape 111 97 116 116 114 95 97 99 99 101 115 115 111 114 32 58 111 102 102 105 99 101 95 97 114 101 97 95 99 111 100 101 escape 111 97 116 116 114 95 97 99 99 101 115 115 111 114 32 58 111 102 102 105 99 101 95 110 117 109 98 101 114 escape 111 return 100 101 102 32 116 101 108 101 112 104 111 110 101 95 110 117 109 98 101 114 escape 111 39 17 40 39 32 43 32 64 111 102 102 105 99 101 95 97 114 101 97 95 99 111 100 101 32 43 32 39 17 41 39 32 43 32 64 111 102 102 105 99 101 95 110 117 109 98 101 114 escape] 0 "%d")) arg)))
-(fset 'refactor-extract-class-1
+(fset 'refactor-extract-class-01
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([71 111 return 99 108 97 115 115 32 84 101 108 101 112 104 111 110 101 78 117 109 98 101 114 escape] 0 "%d")) arg)))
-(fset 'refactor-extract-class-2
+(fset 'refactor-extract-class-02
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([103 103 19 return 32 32 100 101 102 return 79 100 101 102 32 105 110 105 116 105 97 108 105 122 101 escape 111 64 111 102 102 105 99 101 95 116 101 108 101 112 104 111 110 101 32 61 32 84 101 108 101 112 104 111 110 101 78 117 109 98 101 114 46 110 101 119 escape 106 C-return] 0 "%d")) arg)))
-(fset 'refactor-extract-class-3
+(fset 'refactor-extract-class-03
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217747 return 94 99 108 97 115 115 32 84 101 108 101 112 104 111 110 101 return 111 97 116 116 114 95 97 99 99 101 115 115 111 114 32 58 97 114 101 97 95 99 111 100 101 escape] 0 "%d")) arg)))
-(fset 'refactor-extract-class-4
+(fset 'refactor-extract-class-04
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217746 return 64 111 102 102 105 99 101 95 97 114 101 97 95 99 111 100 101 return 106 C-return 106 111 100 101 102 32 111 102 102 105 99 101 95 97 114 101 97 95 99 111 100 101 escape 111 64 111 102 102 105 99 101 95 116 101 108 101 112 104 111 110 101 46 97 114 101 97 95 99 111 100 101 escape] 0 "%d")) arg)))
-(fset 'refactor-extract-class-5
+(fset 'refactor-extract-class-05
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217746 return 64 111 102 102 105 99 101 95 97 114 101 97 95 99 111 100 101 return 120] 0 "%d")) arg)))
-(fset 'refactor-extract-class-6
+(fset 'refactor-extract-class-06
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217747 return 94 101 110 100 return S-return 111 100 101 102 32 111 102 102 105 99 101 95 97 114 101 97 95 99 111 100 101 61 40 97 114 103 escape 111 64 111 102 102 105 99 101 95 116 101 108 101 112 104 111 110 101 46 97 114 101 97 95 99 111 100 101 32 61 32 97 114 103 escape 103 103 19 return 111 102 102 105 99 101 return 100 100] 0 "%d")) arg)))
-(fset 'refactor-extract-class-7
+(fset 'refactor-extract-class-07
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([103 103 134217747 return 94 99 108 97 115 115 32 84 101 108 101 112 104 111 110 101 78 117 109 98 101 114 return 106 65 44 32 58 110 117 109 98 101 114 escape C-return 106 111 100 101 102 32 116 101 108 101 112 104 111 110 101 95 110 117 109 98 101 114 escape 111 39 17 40 39 32 43 32 97 114 101 97 95 99 111 100 101 32 43 32 39 41 39 32 43 32 110 117 109 98 101 114 escape] 0 "%d")) arg)))
-(fset 'refactor-extract-class-8
+(fset 'refactor-extract-class-08
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([103 103 134217747 return 94 32 32 100 101 102 32 116 101 108 101 112 104 111 110 101 95 110 117 109 98 101 114 return 106 83 64 111 102 102 105 99 101 95 116 101 108 101 112 104 111 110 101 46 116 101 108 101 112 104 111 110 101 95 110 117 109 98 101 114 escape] 0 "%d")) arg)))
-(fset 'refactor-extract-class-9
+(fset 'refactor-extract-class-09
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([106 C-return 106 111 100 101 102 32 111 102 102 105 99 101 95 116 101 108 101 112 104 111 110 101 escape 111 64 111 102 102 105 99 101 95 116 101 108 101 112 104 111 110 101 escape] 0 "%d")) arg)))
 (fset 'refactor-extract-class-10
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217746 return 94 32 32 97 116 116 114 return 100 100] 0 "%d")) arg)))
